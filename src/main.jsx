87a260bfb413ddce8cf4762da5903dce7cb7f7eb
dilmurodivcc <zufarovamohira32gmail.com>
2025-03-09 14:10:48 +0500

    Vercel Bag is Solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename='/'>
   <StrictMode>
     <App />
   </StrictMode>
+  </BrowserRouter>
 );
